69f70764717c1e67b55274c8ee1895a35352439a
vitalypolishchuk <vetal.polishuk@gmail.com>
2022-10-10 13:28:59 +0300

    'redux form'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reducers from "../src/reducers";
 import App from "./components/App";
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware()));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
